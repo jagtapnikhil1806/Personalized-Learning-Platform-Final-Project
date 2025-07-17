0445d3a0bd2eab029f87f38500a58a133c41593b
Nikhil Dnyaneshwar Jagtap <jagtapnikhil1806@gmail.com>
2025-07-17 09:29:33 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ app.use(cookieParser());
 app.use(
     cors({
         // origin: 'http://localhost:5173', // frontend link
-        origin: "*",
+        origin: "https://personalized-learning-platform-zeta.vercel.app/",
         credentials: true
     })
 );
